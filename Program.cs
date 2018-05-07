4cf265303b1c966ffe9d2f1452cc1904e19972e3
Bruno Garcia <bruno@brunogarcia.com>
2018-05-07 11:40:43 +0200

    Ugly stack trace

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 #if !NET35
 using System.Threading.Tasks;
@@ -14,18 +15,31 @@ namespace CoreThrower
         static void Main(string[] args)
 #else
         static async Task Main(string[] args)
+
 #endif
         {
             var dsn = Environment.GetEnvironmentVariable("SENTRY_DSN")
-            ?? "https://bd6ec3bf83bf4646952723c20ab16cd3@sentry.garcia.in/1";
+                ?? "https://bd6ec3bf83bf4646952723c20ab16cd3@sentry.garcia.in/1";
 
             System.Console.WriteLine("Using DSN: " + dsn);
 
             var framework = TargetFramework();
             System.Console.WriteLine("Built against: " + framework);
             var client = new RavenClient(dsn);
+            Exception ex = null;
 
-            var ex = new Exception(args.FirstOrDefault() ?? "I love throwing.");
+#if NET40 || NET35
+            ex = new Exception(args.FirstOrDefault() ?? "I love throwing.");
+#else
+            try
+            {
+                var neverSet = await Thrower(new List<Program>());
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+#endif
             ex.Data.Add("AppBuiltTargetingFramework", framework);
 
 #if NET40 || NET35
@@ -36,39 +50,54 @@ namespace CoreThrower
             Console.WriteLine("Event id: " + id);
         }
 
+#if !NET40 && !NET35
+        private static async Task<int> Thrower(IEnumerable<Program> programs)
+        {
+#if !NET45
+            await Task.Yield();
+#else
+            await Task.Delay(0);
+#endif
+
+            return LocalFunction();
+
+            int LocalFunction() => throw null;
+        }
+#endif
+
         private static string TargetFramework()
             =>
-            #if NET35
+#if NET35
             "net35"
-            #elif NET40
+#elif NET40
             "net40"
-            #elif NET45
+#elif NET45
             "net45"
-            #elif NET451
+#elif NET451
             "net451"
-            #elif NET452
+#elif NET452
             "net452"
-            #elif NET46
+#elif NET46
             "net46"
-            #elif NET461
+#elif NET461
             "net461"
-            #elif NET462
+#elif NET462
             "net462"
-            #elif NET47
+#elif NET47
             "net47"
-            #elif NET471
+#elif NET471
             "net471"
-            #elif NET472
+#elif NET472
             "net472"
-            #elif NETCOREAPP2_0
+#elif NETCOREAPP2_0
             "netcoreapp2.0"
-            #elif NETCOREAPP1_1
+#elif NETCOREAPP1_1
             "netcoreapp1.1"
-            #elif NETCOREAPP1_0
+#elif NETCOREAPP1_0
             "netcoreapp1.0"
-            #else
+#else
             "Other"
-            #endif
+#endif
             ;
     }
 }
